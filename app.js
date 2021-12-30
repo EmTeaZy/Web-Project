a0e5d08d2dc608f5513a3b5f32b279729784985e
Talha Zafar <talhabhai8@gmail.com>
2021-12-30 22:36:53 +0500

    FIxed some stuff for the account verification

--- app.js
+++ app.js
@@ -56,4 +56,4 @@ app.use("/users", require("./routes/users.js"));
 
 const PORT = 5000;
 
-app.listen(PORT, console.log(`Server running on  ${PORT}`));
+app.listen(PORT, console.log(`Server running on  localhost:${PORT}`));
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
-const nodemailer = require('nodemailer');
-const jwt = require('jsonwebtoken');
-const _ = require('lodash');
-const xoauth2 = require('xoauth2');
-const EMAIL_SECRET = 'asdf1093KMnzxcvnkljvasdu09123nlasdasdf';
-const { google } = require('googleapis');
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
+const EMAIL_SECRET = "asdf1093KMnzxcvnkljvasdu09123nlasdasdf";
+const { google } = require("googleapis");
 
 // Setting values for email parameter variables
-const CLIENT_ID = '437071356780-ai3jjkfgrs1auhg2u1o81pfn98s3aci4.apps.googleusercontent.com';
-const CLEINT_SECRET = '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';
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//04CwhQWJH65xMCgYIARAAGAQSNwF-L9Ir4MIFU1sI1en4ETyeTho3S6iu2AvaQ-zBl1DGagAh98P9DjWscDf0miE84_dbctvoX1Q';
+const CLIENT_ID = "437071356780-ai3jjkfgrs1auhg2u1o81pfn98s3aci4.apps.googleusercontent.com";
+const CLEINT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN = "1//04CwhQWJH65xMCgYIARAAGAQSNwF-L9Ir4MIFU1sI1en4ETyeTho3S6iu2AvaQ-zBl1DGagAh98P9DjWscDf0miE84_dbctvoX1Q";
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
@@ -23,31 +21,29 @@ const oAuth2Client = new google.auth.OAuth2(
 
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
-async function sendMail(url, { email, name } ) {
+async function sendMail(url, { email, name }) {
   try {
     const accessToken = await oAuth2Client.getAccessToken();
 
-
-const transport = nodemailer.createTransport({
-  service: 'gmail',
+    const transport = nodemailer.createTransport({
+      service: "gmail",
       auth: {
-        type: 'OAuth2',
-        user: 'the.ot.classroom@gmail.com',
+        type: "OAuth2",
+        user: "the.ot.classroom@gmail.com",
         clientId: CLIENT_ID,
         clientSecret: CLEINT_SECRET,
         refreshToken: REFRESH_TOKEN,
         accessToken: accessToken,
       },
-    });    
+    });
 
     const mailOptions = {
-      from: 'OT Classroom <the.ot.classroom@gmail.com>',
+      from: "OT Classroom <the.ot.classroom@gmail.com>",
       to: String(email),
-      subject: 'Email Verification',
+      subject: "Email Verification",
       html: `<h1>Hey there, ${name}!</h1> Thank you for signing up for OT Classroom! <br> Please click <a href="${url}">here</a> email to confirm your email. <br><br> OT Classroom `,
-      
-    };        
-    
+    };
+
     const result = await transport.sendMail(mailOptions);
     return result;
   } catch (error) {
@@ -56,56 +52,58 @@ const transport = nodemailer.createTransport({
 }
 
 // Load User model
-const User = require('../models/User');
-const { forwardAuthenticated } = require('../config/auth');
+const User = require("../models/User");
+const { forwardAuthenticated } = require("../config/auth");
 
 // Login Page
-router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
+router.get("/login", forwardAuthenticated, (req, res) => res.render("login"));
 
 // Register Page
-router.get('/register', forwardAuthenticated, (req, res) => res.render('register'));
+router.get("/register", forwardAuthenticated, (req, res) =>
+  res.render("register")
+);
 
 // Register
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   const { name, email, password, password2 } = req.body;
   let errors = [];
 
   if (!name || !email || !password || !password2) {
-    errors.push({ msg: 'Please enter all fields' });
+    errors.push({ msg: "Please enter all fields" });
   }
 
   if (password != password2) {
-    errors.push({ msg: 'Passwords do not match' });
+    errors.push({ msg: "Passwords do not match" });
   }
 
   if (password.length < 6) {
-    errors.push({ msg: 'Password must be at least 6 characters' });
+    errors.push({ msg: "Password must be at least 6 characters" });
   }
 
   if (errors.length > 0) {
-    res.render('register', {
+    res.render("register", {
       errors,
       name,
       email,
       password,
-      password2
+      password2,
     });
   } else {
-    User.findOne({ email: email }).then(user => {
+    User.findOne({ email: email }).then((user) => {
       if (user) {
-        errors.push({ msg: 'Email already exists' });
-        res.render('register', {
+        errors.push({ msg: "Email already exists" });
+        res.render("register", {
           errors,
           name,
           email,
           password,
-          password2
+          password2,
         });
       } else {
         const newUser = new User({
           name,
           email,
-          password
+          password,
         });
 
         bcrypt.genSalt(10, (err, salt) => {
@@ -114,34 +112,24 @@ router.post('/register', (req, res) => {
             newUser.password = hash;
             newUser
               .save()
-              .then(user => {
+              .then((user) => {
                 req.flash(
-                  'success_msg',
-                  'You are now registered and can log in'
+                  "success_msg",
+                  "You are now registered and can log in"
                 );
               })
-              .catch(err => console.log(err));
+              .catch((err) => console.log(err));
 
             // Sending an email to the user with a confirmation link
-            // async email
-            jwt.sign(
-              {
-                user: _.pick(user, 'id'),
-              },
-              EMAIL_SECRET,
-              {
-                expiresIn: '1d',
-              },
-              (err, emailToken) => {
+            const emailToken = jwt.sign(email, EMAIL_SECRET);
 
-                const url = `http://localhost:5000/users/confirmed/${emailToken}`;
+            const url = `http://localhost:5000/users/confirmed/${emailToken}`;
 
-                sendMail(url, newUser)
-                .then(() => console.log('Email sent successfully'))
-                .catch((error) => console.log(error.message));
-              },
-            );
-            res.redirect('/users/confirmation');
+            sendMail(url, newUser)
+              .then(() => console.log("Email sent successfully"))
+              .catch((error) => console.log(error.message));
+
+            res.redirect("/users/confirmation");
           });
         });
       }
@@ -150,32 +138,29 @@ router.post('/register', (req, res) => {
 });
 
 // Redirecting user to the confirm email page
-router.get('/confirmation', (req, res) => res.render('confirmEmail'))
+router.get("/confirmation", (req, res) => res.render("confirmEmail"));
 
-
-router.get('/confirmed/:token', async (req, res) => {
-
-  console.log('User here')
-  const { user: { id } } = jwt.verify(req.params.token, EMAIL_SECRET);
-  await User.updateOne({ confirmed: true }, { where: { id } });
-  
-  res.render('confirmed')
+router.get("/confirmed/:token", async (req, res) => {
+  let theMail
+  jwt.verify(req.params.token, EMAIL_SECRET, (err, email) => theMail=email);  
+  await User.updateOne({ email: theMail }, { isConfirmed: true});
+  res.render("confirmed");
 });
 
 // Login
-router.post('/login', (req, res, next) => {
-  passport.authenticate('local', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/users/login',
-    failureFlash: true
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true,
   })(req, res, next);
 });
 
 // Logout
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
   req.logout();
-  req.flash('success_msg', 'You are logged out');
-  res.redirect('/users/login');
+  req.flash("success_msg", "You are logged out");
+  res.redirect("/users/login");
 });
 
 module.exports = router;
